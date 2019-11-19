eae19930da0b493ad202f01627593755410a31f0
Kieran Winfield <K.Winfield@ucas.ac.uk>
2019-11-19 13:54:58 +0000

    The results of the expressions now get added to a dictionary, for easier lookup

--- EmailExtraction/Program.cs
+++ EmailExtraction/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 namespace EmailExtraction
 {
@@ -8,13 +9,24 @@ namespace EmailExtraction
     {
         static void Main(string[] args)
         {
-            Regex rx = new Regex(@"@softwire.com\s",
+            //Varaibles needed
+            string OpenFile = File.ReadAllText("C:\\Work\\Training\\EmailExtraction\\Sample.txt");
+            Dictionary<string, int> addresses = new Dictionary<string, int>();
+
+            //Set up Regular Expressions, needed to find what addresses
+            Regex softwireRX = new Regex(@"@softwire.com\s",
+                RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            Regex corndelRX = new Regex(@"@corndel.com\s",
                 RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
-            string OpenFile = File.ReadAllText("C:\\Work\\Training\\EmailExtraction\\Sample.txt");
-            MatchCollection matches = rx.Matches(OpenFile);
+            //Use the expressions to find matches
+            MatchCollection matchesSoftwire = softwireRX.Matches(OpenFile);
+            MatchCollection matchesCorndel = corndelRX.Matches(OpenFile);
 
-            Console.WriteLine(matches.Count);
+            addresses.Add("Softwire", matchesSoftwire.Count);
+            addresses.Add("Corndel", matchesCorndel.Count);
+
+            Console.WriteLine();
             Console.ReadLine();
         }
     }
