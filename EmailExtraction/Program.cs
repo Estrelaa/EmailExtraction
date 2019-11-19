c57b9e63480f9be3c6c42417163a55c9ff46169a
Kieran Winfield <K.Winfield@ucas.ac.uk>
2019-11-19 12:02:16 +0000

    Program now finds strings containing @softwire.com

--- EmailExtraction/Program.cs
+++ EmailExtraction/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace EmailExtraction
 {
@@ -6,7 +7,28 @@ namespace EmailExtraction
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int Counter = 0;
+            string OpenFile = File.ReadAllText("C:\\Work\\Training\\EmailExtraction\\Sample.txt");
+            string[] TextFromFile = OpenFile.Split();
+
+
+            for (int i = 0; i < TextFromFile.Length; i++ )
+            {
+                string temp = TextFromFile.GetValue(i).ToString();
+                if (temp.Length > 13)
+                {
+                    temp.Substring(temp.Length - 13);
+
+                    if (temp.Contains("@softwire.com"))
+                    {
+                        Counter++;
+                        Console.WriteLine(Counter);
+                    }
+                }
+                
+            }
+
+            Console.ReadLine();
         }
     }
 }
