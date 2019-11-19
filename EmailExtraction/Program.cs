127b57c494802374aaf6b96d821232f2f29147b4
Kieran Winfield <K.Winfield@ucas.ac.uk>
2019-11-19 14:48:49 +0000

    Added more comments to explain the code and testing on the Regex

--- EmailExtraction/Program.cs
+++ EmailExtraction/Program.cs
@@ -14,10 +14,12 @@ namespace EmailExtraction
             Dictionary<string, int> addresses = new Dictionary<string, int>();
 
             //Set up Regular Expressions, needed to find what addresses
-            Regex softwireRX = new Regex(@"@\s",
+            Regex softwireRX = new Regex(@"@corndel.com\s",
                 RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
-            //Use the expressions to find matches then update our dictionary
+            /*Use the expressions to find matches then update our dictionary 
+             * depending on if they are already in it or not
+             */
             foreach(Match matchesSoftwire in softwireRX.Matches(OpenFile))
             {
                 string temp = matchesSoftwire.Value;
@@ -31,6 +33,7 @@ namespace EmailExtraction
                 }
             }
 
+            // print the addresses and how many times they appeared
             foreach (KeyValuePair<string, int> key in addresses)
             {
                 Console.WriteLine("{0}", key);
